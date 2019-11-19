6651cd927948b677d6e1c87977b5d27aba09e308
Katie Fox <kfox20@umd.edu>
2019-11-19 19:12:32 +1100

    bottom nav bar w/ recipes (4 total)

---
+++ app/src/main/java/com/example/infs_project/ui/recipes/RecipesFragment.java
@@ -0,0 +1,38 @@
+package com.example.infs_project.ui.recipes;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.infs_project.R;
+
+public class RecipesFragment extends Fragment {
+
+    private RecipesViewModel mViewModel;
+
+    public static RecipesFragment newInstance() {
+        return new RecipesFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.recipes_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(RecipesViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/infs_project/ui/recipes/RecipesViewModel.java
@@ -0,0 +1,7 @@
+package com.example.infs_project.ui.recipes;
+
+import androidx.lifecycle.ViewModel;
+
+public class RecipesViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
